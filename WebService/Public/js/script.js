75cd5aca11af0f29ea84a1acda55c90817532bc5
Ruofei Du <ruofei@cs.umd.edu>
2014-05-14 15:56:59 -0400

    57aMP

--- WebService/Public/js/script.js
+++ WebService/Public/js/script.js
@@ -1,39 +1,68 @@
+var lock = false; 
+var localhost = true; 
+var word = "11111111111111100001100000101100110";
+var baseURL = "http://duruofei.com/Stamp/";
+var localURL = "http://localhost:8888/Stamp/";
+
+var lastURL = "?last="; 
+var setURL = "?set="; 
+
 function UpdateViz() {
 	var data = $("#data").val() ; 
 	console.log( data ); 
 
-	var n = 5, m = 7; 
+	if ($("#data").val() != word) {
+		$("#data").val(word) ; 
+		var n = 5, m = 7; 
 
-	for (var i = 0; i < n; ++i) {
-		for (var j = 0; j < m; ++j) {
-			var cid = (i+1)*10 + (j+1);
-			//console.log(cid); 
-			$("#g"+ cid).fadeTo("slow", 1 - parseInt(data.substr(i*m+j, 1)) );
+		for (var i = 0; i < n; ++i) {
+			for (var j = 0; j < m; ++j) {
+				var cid = (i+1)*10 + (j+1);
+				console.log(parseInt(data.substr(i*m+j, 1))); 
+				$("#g"+ cid).fadeTo("slow", 1 - parseInt(data.substr(i*m+j, 1)) );
+			}
 		}
 	}
 }
 
 function UpdateData() {
-	$.ajax({url:"http://duruofei.com/ThermalGrid/?last=1", success:function(result){
-    	$("#data").val(result);
-  	}});
+	if (!lock) {
+		$.ajax({url:(baseURL + lastURL + (256-parseInt($( "#timestamp" ).slider( "value" ) ) ) ), success:function(result){
+		    	word = result;
+		    	lock = false; 
+		  	}, error: function(XMLHttpRequest, textStatus, errorThrown) { 
+	        	lock = false; 
+	    	}   
+    	});
+  	}
 }
 
 function UpdateThres() {
-	$.ajax({url:("http://duruofei.com/ThermalGrid/?set=" + $( "#slider" ).slider( "value" )), success:function(result){
-    	console.log(result);
+	$.ajax({url:(baseURL + setURL + $( "#thres" ).slider( "value" )), success:function(result){
+    	//console.log(result);
   	}});
+
+  	$("#lblThres").html("<h3>Threshold: " + $( "#thres" ).slider( "value" )  + "</h3>"); 
+}
+
+function UpdateTimeStamp() {
+	//console.log("<h3>Time stamp: " + (256-parseInt($( "#timestamp" ).slider( "value" ) )) + "</h3>"); 
+	$("#lblTimeStamp").html("<h3>Time stamp: " + (256-parseInt($( "#timestamp" ).slider( "value" ) )) + "</h3>"); 
 }
 
 $(document).ready(function() {
 	UpdateViz(); 
+	if (localhost) {
+		baseURL = localURL; 
+	}
 
 	setInterval(function() {
     	UpdateData();
     	UpdateViz(); 
+    	console.log(256-parseInt($( "#timestamp" ).slider( "value" ))); 
 	}, 500); 
 
-	$( "#slider" ).slider({
+	$( "#thres" ).slider({
       orientation: "horizontal",
       range: "min",
       max: 255,
@@ -41,7 +70,21 @@ $(document).ready(function() {
       slide: UpdateThres,
       change: UpdateThres
     });
-    $( "#slider" ).slider( "value", 127 );
+
+	$( "#timestamp" ).slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 255,
+      slide: UpdateTimeStamp,
+      change: UpdateTimeStamp
+    });
+
+
+    $( "#timestamp" ).slider( "value", 255 );
+
+
+    $( "#thres" ).slider( "value", 127 );
 });
 
 
--- WebService/index.php
+++ WebService/index.php
@@ -1,8 +1,8 @@
 <?php 
 	define('THINK_PATH', './ThinkPHP/');
 	define('__ROOT__', './');  
-	define('APP_NAME', 'ThermalGrid'); 
-	define('APP_PATH', './ThermalGrid/');
+	define('APP_NAME', '57aMP'); 
+	define('APP_PATH', './57aMP/');
 	define('APP_DEBUG', true);
 	require(THINK_PATH. "ThinkPHP.php");
 	//session_start();
